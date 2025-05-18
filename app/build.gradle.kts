0c56cb3456a9c9adcd4f53160292f2eb2c35afa9
Luciano <luciano.canuto1@gmail.com>
2025-05-18 16:34:18 -0300

    Iniciando Projeto com ViewBinding

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+
+}
+
+android {
+    namespace = "com.luciano.messenger"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.luciano.messenger"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+
+
+    //Dependencias do firebase
+    implementation("com.google.firebase:firebase-analytics")//Analytics
+    implementation("com.google.firebase:firebase-auth")//Autenticação
+    implementation("com.google.firebase:firebase-firestore")//Banco de dados
+    implementation("com.google.firebase:firebase-storage")//Armazeamnto
+
+
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
