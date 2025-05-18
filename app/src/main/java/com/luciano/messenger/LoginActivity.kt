8c975a3f0d53c71a658e7f5ad07f9dbc2a999062
Luciano <luciano.canuto1@gmail.com>
2025-05-18 17:19:02 -0300

    Layout e configuraçoes de iniciais de cores

---
+++ app/src/main/java/com/luciano/messenger/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.luciano.messenger
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.luciano.messenger.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+    }
+}
